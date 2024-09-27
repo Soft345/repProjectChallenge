431eb6a8e637e72fc5387000d9b31f5a8a3ecc95
Soft345 <technosoft.345@gmail.com>
2024-09-27 10:02:17 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Mahasiswa</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/my_style.css">
 </head>
 <body>
