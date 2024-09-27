7186e1dcb446a114e9a0c89a05392d8012df4725
Soft345 <technosoft.345@gmail.com>
2024-09-27 08:58:09 +0700

    Update Database

--- classes/Database.php
+++ classes/Database.php
@@ -1,1 +1,26 @@
+<?php
+class Database {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+    public $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function query($sql) {
+        return $this->conn->query($sql);
+    }
+
+    public function escape_string($value) {
+        return $this->conn->real_escape_string($value);
+    }
+}
+?>
 
