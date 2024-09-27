050babb962d7cb14ef34f3a155b044d3f15c3c99
Soft345 <technosoft.345@gmail.com>
2024-09-27 08:37:24 +0700

    create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include 'config/config.php';
+include 'classes/Database.php';
+include 'classes/Mahasiswa.php';
+include 'includes/header.php';
+
+$db = new Database();
+$mahasiswa = new Mahasiswa($db);
+
+$result = $mahasiswa->read();
+?>
+
+<h2>Data Mahasiswa</h2>
+<a href="create.php">Tambah Data</a>
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>NIM</th>
+        <th>Jurusan</th>
+        <th>Aksi</th>
+    </tr>
+    <?php while ($row = $result->fetch_assoc()) : ?>
+        <tr>
+            <td><?= $row['id']; ?></td>
+            <td><?= $row['nama']; ?></td>
+            <td><?= $row['nim']; ?></td>
+            <td><?= $row['jurusan']; ?></td>
+            <td>
+                <a href="update.php?id=<?= $row['id']; ?>">Edit</a> | 
+                <a href="delete.php?id=<?= $row['id']; ?>" onclick="return confirm('Yakin ingin menghapus?');">Hapus</a>
+            </td>
+        </tr>
+    <?php endwhile; ?>
+</table>
+
+<?php include 'includes/footer.php'; ?>
+
